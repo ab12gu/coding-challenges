3d002c4b60121cdb4783e952b02cf302f97bd068
ab12gu <ab18gu@gmail.com>
2023-08-14 11:01:55 -0700

    add comments to 215 leetcode qn

--- python/leetcode/215-kth-largest-element-in-an-array.py
+++ python/leetcode/215-kth-largest-element-in-an-array.py
@@ -2,22 +2,23 @@
 # by: Abhay Gupta
 # date: 23-08-14
 
-
-
+# class definition
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-      # nums.sort(reverse=True)
-      # return nums[k-1]
 
+    # main method / no constructor
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+      
+      # intialize variables
       low = min(nums)
       high = max(nums)
       occurance_array = [0 for i in range(low,  high+1)]
 
+      # count occurances per element in array
       for i in nums:
         occurance_array[i-low] += 1
 
+      # reverse through occurances array and return the kth largest value
       curr = 0
-
       for i in range(len(occurance_array)-1,-1, -1):
         curr += occurance_array[i]
         if curr >= k:
