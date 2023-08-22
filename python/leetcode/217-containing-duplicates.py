b1fcb34237f876fc20a28fc5d1d766c12c6379f1
ab12gu <ab18gu@gmail.com>
2023-08-21 19:04:19 -0700

    leetcode 217 easy, implemented my own hashmap with linear probe

--- python/leetcode/217-containing-duplicates.py
+++ python/leetcode/217-containing-duplicates.py
@@ -1,6 +1,30 @@
 class Solution:
   def containsDuplicate(self, nums: List[int]) -> bool:
 
+    # implement linear hashmap
+    size = len(nums)
+    if size == 1:
+      return False
+
+    high = max(nums) + 1
+    duplicates = [high] * size
+
+    for i in nums:
+      index = i % size
+
+      while(1):
+        if duplicates[index] == i:
+          print(index, i)
+          return True
+        elif duplicates[index] == high:
+          duplicates[index] = i
+          break
+        index = (index + 1) % size
+
+    return False
+
+    # simple hashmap solution
+
     bucket = {}
     for i in nums:
       if i not in bucket:
