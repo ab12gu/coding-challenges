037102bf962f3f1df1c60667a238a349f054bc5d
ab12gu <ab18gu@gmail.com>
2023-08-22 01:14:35 -0700

    leetcode 1 - easy

---
+++ python/leetcode/1-two-sum.py
@@ -0,0 +1,54 @@
+# filename: 1-two-sum.py
+# by: Abhay Gupta
+# date: 23-08-21
+#
+# description: Given an array of integers nums and an integer target, 
+# return indices of the two numbers such that they add up to target.
+#
+# You may assume that each input would have exactly one solution, 
+# and you may not use the same element twice.
+
+
+class Solution:
+
+  # shortest solution that I could think of O(n)
+  hashmap = {}
+  for c, i in enumerate(nums):
+    hashmap[i] = c
+
+  for c, i in enumerate(nums):
+    if (z := hashmap.get(target - i)):
+      if (z-c):
+        return (c,z)
+
+  # check create a hashmap of expected adjacent pair and its index
+  # memory large because you keep the index of repeats [unnecessary]
+  # runtime: O(n)
+  hashmap = {}
+  for c, i in enumerate(nums):
+    hashmap[i] = [c] + hashmap.get(i, [-1])
+
+  for c, i in enumerate(nums):
+    if (target - i) in hashmap:
+      for j in hashmap[target - i]:
+        if c != j and j >= 0:
+          return (j, c)
+
+  # optimized to use half the array 
+  # runtime: [n], [n-1] ..... 1 + 2 + 3 ... + n = n(n + 1)/2 = n**2
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    
+    for c,i in enumerate(nums):
+      for d,j in enumerate(nums):
+        if c != d:
+          if i + j == target:
+            return [c,d]
+
+  # brute force solution
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+      
+    for c,i in enumerate(nums):
+      for d,j in enumerate(nums):
+        if c != d:
+          if i + j == target:
+            return [c,d
