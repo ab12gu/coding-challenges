eb250c632ddfa57f951a73448d061495a6d8ceb0
ab12gu <ab18gu@gmail.com>
2023-08-26 12:53:22 -0700

    leetcode 646 - refactor and add some memory change alternatives

--- python/leetcode/646-maximum-of-pair-chain.py
+++ python/leetcode/646-maximum-of-pair-chain.py
@@ -4,16 +4,41 @@ class Solution:
 
       chain_lengths = [1] * len(pairs)
       chain_end = [pairs[0]]
-      for i in pairs[1:]:
-        
 
+      # iterate through pairs
+      for i in pairs[1:]:
+       
+        # find length of pair chains prior to current pair
         for c, j in enumerate(chain_end):
           if j[1] < i[0]:
             chain_lengths[c] += 1
             chain_end[c] = i
         
+        # add new pair to prior pairs
         chain_end.append(i)
+    
+      # return longest chain  
+      return max(chain_lengths)
+
+  # alternative solutions
+
+      size = len(pairs)
+      chain_lengths = [1] * size
+
+      for i in range(1,size):
+        for j in range(i):
+          if pairs[j][1] < pairs[i][0]:
+            chain_lengths[j] += 1
+            pairs[j] = pairs[i]
 
       return max(chain_lengths)
 
+      #for c, i in enumerate(pairs[1:]):
+      #  for d, j in enumerate(pairs[:c+1]):
+      #    if j[1] < i[0]:
+      #      chain_lengths[d] += 1
+      #      pairs[d] = i
 
+
+
+       
