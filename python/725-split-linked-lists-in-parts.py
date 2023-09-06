47db20cb5434932a6f782a1862a60e89a294d50d
ab12gu <ab18gu@gmail.com>
2023-09-06 12:02:58 -0700

    Add leetcode 725 - medium solution submission

---
+++ python/725-split-linked-lists-in-parts.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
+
+        arr = [] 
+        while(head):
+            arr.append(head)
+            head = head.next
+
+        split = len(arr) // k
+        remainder = len(arr) % k
+
+        output = [None] * k
+        r = 0
+        for i in range(min(len(arr), k)):
+            if i > 0:
+                arr[i*split+r-1].next = None
+
+            output[i] = arr[i*split+r]
+            if i < remainder:
+                r += 1
+
+        return outpu
