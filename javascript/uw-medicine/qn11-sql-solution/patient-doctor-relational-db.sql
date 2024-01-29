f08f100312aeab135d5f3be52c14d82ee2b53151
ab12gu <ab18gu@gmail.com>
2024-01-29 09:31:01 -0800

    Add solution to sql qn11

---
+++ javascript/uw-medicine/qn11-sql-solution/patient-doctor-relational-db.sql
@@ -0,0 +1,41 @@
+-- 
+-- filename: temp.sql
+-- by: Abhay Gupta
+-- date created: 24-01-26
+--
+-- desc:
+-- 1. How many sql tables do you need to store doctor/patient data?
+-- 2. Write the SQL statements to create the tables and describe what would be stored in each table
+
+
+-- doctor table
+-- the data stores the following doctors' data: 
+l-- - unique ID, first name, last name, practice address
+CREATE TABLE Doctors {
+    DoctorId int,
+    FirstName varchar(20) NOT NULL,
+    LastName varchar(20) NOT NULL,
+    PracticeAddress varchar(255)
+}
+
+-- patient table
+-- the data stores the following patients' data: 
+-- - unique ID, first name, last name, gender, age
+CREATE TABLE Patients {
+    PatientId int, 
+    FirstName varchar(20) NOT NULL,
+    LastName varchar(20) NOT NULL,
+    Gender varchar(1) NOT NULL, 
+    Age int
+}
+
+-- junction table
+-- models the relationship between doctors and patients
+-- the id's and the patient/doctor, their corresponding relevent row, and the parent-child relationship via foreign key
+CREATE TABLE DoctorPatient {
+    DoctorId int,
+    PatientId int, 
+    PRIMARY KEY (DoctorId, PatientId),
+    FOREIGN KEY (DoctorId) REFERENCES Doctors(DoctorId),
+    FOREIGN KEY (PatientId) REFERENCES Patients(PatientId),
+}
