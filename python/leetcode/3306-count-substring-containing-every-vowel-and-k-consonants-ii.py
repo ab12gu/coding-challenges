4556951159299de4bd4a03450b953afe0e80c769
Abhay Gupta <ab18gu@gmail.com>
2025-03-10 16:13:24 -0700

    Add 3306 iterative solution too slow

--- python/leetcode/3306-count-substring-containing-every-vowel-and-k-consonants-ii.py
+++ python/leetcode/3306-count-substring-containing-every-vowel-and-k-consonants-ii.py
@@ -2,6 +2,28 @@ class Solution:
     def countOfSubstrings(self, word: str, k: int) -> int:
         arr = []
         self.vowels = ["a", "e", "i", "o", "u"]
+        total = 0
+
+        for e, i in enumerate(word):
+            vowels = self.vowels[:]
+            constant = 0
+
+            for i in word[e:]:                
+                if i not in self.vowels:
+                    constant += 1
+                    if constant > k:
+                        break
+                else:
+                    if i in vowels:
+                        vowels.pop(vowels.index(i))
+                if not vowels:
+                    if constant == k:
+                        total += 1
+
+        return totalclass Solution:
+    def countOfSubstrings(self, word: str, k: int) -> int:
+        arr = []
+        self.vowels = ["a", "e", "i", "o", "u"]
         self.total = 0
         self.constants = k
 
