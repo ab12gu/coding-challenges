d24b589e5e2d593c00c99caa1f4cace303542312
ab12gu <ab18gu@gmail.com>
2026-06-18 16:34:40 -0700

    Add 125 solution redone

--- python/leetcode/125-valid-palindrome.py
+++ python/leetcode/125-valid-palindrome.py
@@ -1,27 +1,25 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        string = []
 
-      s = s.lower()
-      new_string = ""
-      left = ord('a'); right = ord('z')
+        for i in s:
+            if ord('a') <= ord(i) and ord(i) <= ord('z'):
+                string.append(i)
+        for i in range(int(len(string)/2)):
+            if string[i] != string[-i-1]:
+                return False
 
-      for i in s:
-        if ord(i) >= left and ord(i) <= right:
-          new_string += i
-        elif i.isdigit():
-          new_string += i
-
-      for i in range(len(new_string) // 2):
-        if new_string[i] != new_string[-(i + 1)]:
-          return False
-      return True
-
-    def isPalindromeFaster(self, string):
-        string = string.lower()
+        return True
 
         string = ''.join([s for s in string if s.isalnum()])
-
         return string == string[::-1]
 
+if __name__ == '__main__':
 
+    string = "A man, a plan, a canal: Panama"
 
+    new_class = Solution()
+    boolean = new_class.isPalindrome(string)
+
+    print(boolean)
