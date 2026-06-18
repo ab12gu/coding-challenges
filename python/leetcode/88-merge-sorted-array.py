fe9620d3c5e99b3f507f5d3e4fff210b1c52dc97
ab12gu <ab18gu@gmail.com>
2026-06-18 13:51:18 -0700

    Leetcode 88 - sort library to solve

--- python/leetcode/88-merge-sorted-array.py
+++ python/leetcode/88-merge-sorted-array.py
@@ -4,7 +4,10 @@ class Solution:
     def __init__(self):
         pass
     def merge(self, nums1, m, nums2, n):
-        print(nums1, m, nums2, n)
+
+        nums1[m:] = nums2
+        nums1.sort()
+        print(nums1)
 
 if __name__ == "__main__":
     class_instance = Solution()
