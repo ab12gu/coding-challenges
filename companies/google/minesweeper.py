588a656344eeff9a47f9a1a0b8eee20f4fac8c76
ab12gu <ab18gu@gmail.com>
2026-06-19 12:37:09 -0700

    Google Prep: 1 direction minesweeper, got concept for recursion

--- companies/google/minesweeper.py
+++ companies/google/minesweeper.py
@@ -26,12 +26,30 @@ class Matrix:
     def setmatrix(self, row, col):
         if len(grid) <= row and len(grid[0]) <= col:
             grid[row,col] = True
-   
+
+    def click(self, row, col):
+        if row <= self.rows and col <= self.cols:
+            i,j = row + 1, col
+            print(self.rows,i)
+            if i <= self.rows:
+                if self.grid[i][j] == False:
+                    self.grid[i][j] = 0
+                    self.click(i,j)
+                if self.grid[i][j] == True:
+                    self.grid[i][j] = 1
+                    self.click(i,j)
+
+    def print(self):
+        print(self.grid)
+                    
 
 if __name__ == '__main__':
 
     matrix = Matrix();
     matrix.resize(2, 3)
+    matrix.click(0, 0)
+    matrix.print()
+
 
 
 
