c8d726a8ff26081e0ca374fe04df57a9e9fbde38
ab12gu <ab18gu@gmail.com>
2026-06-18 19:31:59 -0700

    Google Compression: went too in weeds, need reset

--- companies/google/compression-decompression.py
+++ companies/google/compression-decompression.py
@@ -3,33 +3,60 @@
 # https://www.google.com/about/careers/applications/candidate-prep/swe?
 
 class Data:
-    def __init__(self):
+
+    def __init__(self, string):
+        self.string = string
         pass
 
     def compression(self):
         pass
 
-    def decompression(self, string):
-        substring = ""
-        outputstring = ""
-        number = False
-        repeat = ""
-        for i in string:
-            if i == "[":
-                number = True
-                repeat = int(repeat)
-            if i.isdigit() and number == False:
-                repeat += i
-                pass
+    def decompress(num):
+        while self.string:
+
+            i = self.string.pop()
+            substring = ""
+            outputstring = ""
+
             if i.isalpha():
                 substring += i
-                pass
             if i == "]":
+                print("HELLO")
                 for j in range(repeat):
                     outputstring += substring
                 substring = ""
                 number = False
                 repeat = ""
+
+            print(outputstring)
+
+            return substring
+
+    def decompression(self):
+        outputstring = ""
+        number = False
+        substring = ""
+        repeat = ""
+
+        print(self.string)
+
+        self.string = self.string.split().reverse()
+        
+        print(self.string)
+        subs = ""
+        while self.string:
+            i = self.string.pop()
+            print(i)
+            if i == "[":
+                number = True
+                repeat = int(repeat)
+                outputstring += decompress(repeat)
+            if i == isdigit() and number == False:
+                repeat += i
+            if i.isalpha():
+                substring += i
+
+        print(substring)
         if substring:
             outputstring += substring
 
@@ -39,8 +66,8 @@ if __name__ == '__main__':
 
     string = '300[abc]4[ab]c'
     
-    data = Data()
-    string = data.decompression(string)
+    data = Data(string)
+    string = data.decompression()
     print(string)
 
     
