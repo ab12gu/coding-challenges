8516194ae902f3550ac5101dc2a8573f58fe1857
ab12gu <ab18gu@gmail.com>
2026-06-21 22:40:58 -0700

    215 solution slow, stupid sort

--- python/leetcode/215-kth-longest-element.py
+++ python/leetcode/215-kth-longest-element.py
@@ -1,5 +1,17 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort()
+        heap = [nums[0]]
 
-        return nums[-k
+        for i in range(1,len(nums)):
+            flag = False
+            for j in range(len(heap)):
+
+                print(i,j, heap)
+                if nums[i] >= heap[j]:
+                    heap = heap[:j] + [nums[i]] + heap[j:]
+                    flag = True
+                    break
+            if flag == False:
+                heap.append(nums[i])
+        print(heap)
+        return heap[k-1]
